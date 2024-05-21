92e672ac7aa93acf884c1c8cb310e8c38700ac00
Trick-Breno <141360060+Trick-Breno@users.noreply.github.com>
2024-05-21 20:57:17 -0300

    Delete codigo-fonte/tela inicial directory

--- codigo-fonte/tela inicial/tela-ofc.js
+++
@@ -1,9 +0,0 @@
-let filtro = document.querySelector("#filtro");
-
-filtro.onmouseup = function(){
-    ItensFiltro.style.visibility = "visible";
-};
-
-filtro.onmouseout = function(){
-    ItensFiltro.style.visibility = "hidden";
-};
